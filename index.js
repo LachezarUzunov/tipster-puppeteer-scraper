4da065e283fcc21aba977c691ac1dba04667f450
Lachezar <uzunov_89_lacho@abv.bg>
2023-05-09 10:29:27 +0300

    scraping countries and leagues refactoring

--- index.js
+++ index.js
@@ -13,41 +13,50 @@ const { scrapingGames } = require('./scraping/scrapingGames');
 
 // Connecting to the mySQL databsae
 connectDB()
-
 let browser;
 
 const url = 'https://tipster.bg/statistika';
 const secondUrl = 'https://tipster.bg'
 
+browser = await puppeteer.launch({ headless: false });
+const page = await browser.newPage();
+
 async function sleep(miliseconds) {
     return new Promise(resolve => setTimeout(resolve, miliseconds))
 }
 
-async function main() {
-    browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+// 1. Scraping countries
+async function countries() {
+    await scrapingCountries(url, page)
+}
+ countries()
 
-    // Scraping countries and getting league URLs
-    const leagueUrls = await scrapingCountries(url, browser)
-  // console.log(leagueUrls)
-    // Scraping leagues and teams for each league URL;
-    for (let i = 0; i < leagueUrls.length; i++) {
-       const league = await scrapingLeagues(leagueUrls[i], page);
-      // await scrapingTeams(leagueUrls[i], page, league);
+// 2. Scraping leagues
+ async function leagues() {
+    try {
+        await page.goto(url, { waitUntil: 'networkidle2' })
+        const html = await page.evaluate(() => document.body.innerHTML);
+        const $ = cheerio.load(html);
 
-       await sleep(100);
+        const leagueUrls = $('.dce').map((index, element) => {
+            return 'https://tipster.bg' + ($(element).find('a').attr('href'))
+        }).get();
+    
+        for (let i = 0; i < leagueUrls.length; i++) {
+            await scrapingLeagues(leagueUrls[i], page);
+        }
+    } catch (error) {
+        console.log(error)
     }
- }
+}
+leagues()
+
 
  async function gamesUrls() {
     let gamesUrls
-    browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
     await page.goto(secondUrl, { waitUntil: "networkidle2" });
     const html = await page.evaluate(() => document.body.innerHTML);
     const $ = cheerio.load(html);
-
-    
     await scrapingGames(secondUrl, page);
  }
 
@@ -73,11 +82,9 @@ StandardMarket.belongsTo(Game, {
     foreignKey: 'id'
 })
 
-main();
-
 const createTable = async () => {
     try {
-        const res = await StandardMarket.sync();
+        const res = await Country.sync();
         console.log('Table and model synced successfully')
     } catch (err) {
         console.log('Error syncing the table and the model')
--- scraping/scrapingCountries.js
+++ scraping/scrapingCountries.js
@@ -1,10 +1,9 @@
 const cheerio = require('cheerio');
 const { savingCountries } = require('../controller/FixturesController');
 
-async function scrapingCountries(url, browser) {
+async function scrapingCountries(url, page) {
     let countries = []
     try {
-        const page = await browser.newPage();
         await page.goto(url, { waitUntil: 'networkidle2' })
         const html = await page.evaluate(() => document.body.innerHTML);
         const $ = cheerio.load(html);
@@ -13,18 +12,11 @@ async function scrapingCountries(url, browser) {
             return ($(element).text().split(' - ')[0]);
         }).get();
     
-        const leagueUrls = $('.dce').map((index, element) => {
-            return 'https://tipster.bg' + ($(element).find('a').attr('href'))
-        }).get();
-    
         countries = countriesResult.filter((c, i) => c !== countriesResult[i + 1]);
         // ----- Saving countries to database --------- //
         for (let i = 0; i < countries.length; i++) {
             await savingCountries(countries[i]);
         }
-      
-        leagueUrls.shift();
-        return leagueUrls;
     } catch (error) {
         console.log(error)
     }
